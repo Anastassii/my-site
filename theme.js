a2a7dde93c902c1516449ca51d0c9185290c2e5c
Anastassii <77493573+Anastassii@users.noreply.github.com>
2021-01-16 16:59:33 +0300

    Add files via upload

--- theme.js
+++ theme.js
@@ -1,7 +1,7 @@
+document.querySelector('.page');
 let page = document.querySelector('.page');
-
 let themeButton = document.querySelector('.theme-button');
-themeButton.onclick = function(click) {
+themeButton.onclick = function() {
   page.classList.toggle('dark-theme');
   page.classList.toggle('white-theme');  
 };
